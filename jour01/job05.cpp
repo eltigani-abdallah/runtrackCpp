4b8d33c2e50ad3ab1ad626a4cb4cdb25c197c9a8
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-05-13 11:26:39 +0200

    feat: job05

---
+++ jour01/job05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int multiplication(int a, int b) {
+    return a * b;
+}
+
+int main() {
+
+    int wow=5;
+    int wowie=6;
+
+    std::cout << multiplication(wow, wowie) << std::endl;
+}
