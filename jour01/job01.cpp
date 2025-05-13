b0c33e4845237c39db4bdb23b5f0577906905ac7
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-05-13 11:02:23 +0200

    feat: job 1

--- jour01/job01.cpp
+++ jour01/job01.cpp
@@ -1,2 +1,26 @@
-#include "job01.h"
 
+
+context:
+
+char c = '\x01';
+short int p= 10;
+
+quels sont le types et valuers de chacune des expressions suivantes:
+
+x= p+3
+
+y=c+1
+
+z=p+c
+
+w= 3*p+5*c
+
+SOLUTIONS:
+
+1) short int x= 13
+
+2) int y= 2
+
+3) int z= 11
+
+4) int w = 35
