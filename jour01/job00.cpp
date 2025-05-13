5ab1d320134b1df3093d0b08d697cf2c3a475969
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-05-13 10:48:23 +0200

    feat: job 0

---
+++ jour01/job00.cpp
@@ -0,0 +1,23 @@
+eliminer les paretheses superflues:
+
+1) a=(x+5)
+
+2) a=(x=y)+2
+
+3) a=(x==y)
+
+4) (a<b)&&(c>d)
+
+5) (i++)*(n+p)
+
+SOLUTIONS:
+
+1) a=x+5
+
+2) a=(x=y)+2
+
+3) a= (x==y)
+
+4) a<b && c<d
+
+5) (i++)*(n+p)
