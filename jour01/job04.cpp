bfe46d08bc2b7ff831dd6e3120cbf3d43baf21e5
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-05-13 11:23:54 +0200

    feat: job04

---
+++ jour01/job04.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int addition(int a, int b) {
+    return a + b;
+}
+
+
+int main() {
+    int firstnum=5;
+    int secondnum=10;
+
+    std::cout<<addition(firstnum, secondnum)<<std::endl;
+
+
+
+    return 0;
+}
